051ed601429fce02883f5f9b2a1da841f8dbadbf
Tomas Perez <tomasitopg6@gmail.com>
2020-02-02 19:51:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,13 +13,12 @@ function nestedTarget(){
   
   //Define a function increaseRankBy(n) that increases the ranks in all of the .ranked-lists by n
   
-  function increaseRankBy(n){
-   const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
-
-    for (let i = 0; i < upRank.length; i++){
-     upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
-     }
-  }
+function increaseRankBy(n) {
+   var ranks = document.querySelectorAll('ul.ranked-list li');
+   for (var i = 0; i < ranks.length; i++) {
+     ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+   }
+ }
   
   //Define a function deepestChild() that pulls out the most deeply nested child element from div#grand-node
   
