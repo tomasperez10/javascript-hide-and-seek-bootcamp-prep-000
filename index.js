a4e4a1d73a1298470fea00ecc37d403e8490e281
Tomas Perez <tomasitopg6@gmail.com>
2020-02-02 19:51:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+//Define a function getFirstSelector(selector) that accepts a selector and returns the first element that matches
 
+function getFirstSelector(selector){
+   return document.querySelector(selector)
+  }
+
+
+//Define a function nestedTarget() that pulls a .target out of #nested. Note: #nested = div and .target = div
+
+function nestedTarget(){
+   return document.getElementById('nested').querySelector('div.target')
+  }
+  
+  //Define a function increaseRankBy(n) that increases the ranks in all of the .ranked-lists by n
+  
+function increaseRankBy(n) {
+   var ranks = document.querySelectorAll('ul.ranked-list li');
+   for (var i = 0; i < ranks.length; i++) {
+     ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+   }
+ }
+  
+  //Define a function deepestChild() that pulls out the most deeply nested child element from div#grand-node
+  
+  function deepestChild(){
+   var deep = document.getElementById('grand-node').querySelectorAll('div')
+   return deep[deep.length-1]
+  }
