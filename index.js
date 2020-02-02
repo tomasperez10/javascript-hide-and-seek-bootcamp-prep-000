f00053d605ba4931a657077c8b225e7fdc09cc7b
Tomas Perez <tomasitopg6@gmail.com>
2020-02-02 19:32:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 function getFirstSelector(selector){
    return document.querySelector(selector)
-
   }
 
 
 //Define a function nestedTarget() that pulls a .target out of #nested. Note: #nested = div and .target = div
 
+function nestedTarget(){
+   return document.getElementById('nested').querySelector('div.target')
+  }
