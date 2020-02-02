04f5126dd2423e8b6ac9f216797353b26333b76a
Tomas Perez <tomasitopg6@gmail.com>
2020-02-02 19:32:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-//accepts a selector and returns the first element that matches
+//Accepts a selector and returns the first element that matches
 
 function getFirstSelector(selector){
    return document.querySelector(selector)
 
   }
 
+
+//Define a function nestedTarget() that pulls a .target out of #nested. Note: #nested = div and .target = div
 
